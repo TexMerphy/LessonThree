3b9c882788bb59cd536d6fa3ad9b5593f5ed1e8a
TexMerph <loshakovpv@yandex.ru>
2021-07-26 19:13:00 +0300

    Task 2 begin

---
+++ src/main/java/ru/geekbrains/lesson/three/WordPuzzle.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson.three;
+
+import java.util.Scanner;
+
+public class WordPuzzle {
+    String[] words = {"apple", "orange", "lemon", "banana",
+            "apricot", "avocado", "broccoli", "carrot", "cherry",
+            "garlic", "grape", "melon", "leak", "kiwi", "mango",
+            "mushroom", "nut", "olive", "pea", "peanut", "pear",
+            "pepper", "pineapple", "pumpkin", "potato"};
+    String usrAns;
+    Scanner
+
+}
